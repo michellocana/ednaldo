f8bf8a9fb1f1649f60b536e88113614fced6cdf1
Michell Ocaña <ocana.1997@hotmail.com>
2020-05-17 01:44:47 -0300

    feat: adding args validation

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,31 @@ use term_size::dimensions as get_terminal_dimensions;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // TODO make "ednaldo" command only work with "pereira" arg
+    let args = std::env::args().collect::<Vec<String>>();
+    let has_lastname_arg = args.len() == 2;
+    let is_ednaldo_pereira = has_lastname_arg && String::from(&args[1]).to_lowercase() == "pereira";
+    let has_correct_spell = has_lastname_arg && &args[1] == "Pereira";
+
+    if !has_lastname_arg {
+        print_error("Você precisa digitar o sobrenome de Ednaldo Pereira pra termos certeza que você quer ver uma imagem de Ednaldo Pereira".into())
+    }
+
+    if !is_ednaldo_pereira {
+        print_error(
+            format!("Você tentou exibir uma mensagem do Ednaldo {}, mas esse comando só mostra imagens do Ednaldo Pereira", &args[1]).into(),
+        )
+    }
+
+    if !has_correct_spell {
+        print_error(
+            format!(
+                "\"{}\" não é a pronúncia certa do sobrenome de Ednaldo Pereira",
+                &args[1]
+            )
+            .into(),
+        )
+    }
+
     let image_list: ImageList = match get_image_list().await {
         Ok(image_list) => image_list,
         Err(err) => print_error(err),
